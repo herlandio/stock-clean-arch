3c30ee5cd6df1a2ade3e1c3f768114adc1d135f4
Herlandio <herlandio7@gmail.com>
2024-11-18 20:15:48 -0300

    feat: added smart alerts

--- src/main/java/com/herlandio7/stock/infra/gateways/CheckCriticalStockGateway.java
+++ src/main/java/com/herlandio7/stock/infra/gateways/CheckCriticalStockGateway.java
@@ -18,9 +18,12 @@ import lombok.extern.slf4j.Slf4j;
 public class CheckCriticalStockGateway implements ICheckCriticalStock {
     
     private final IProductGateway productGateway;
-    private static final String TOPIC = "low-stock-notifications";
     private final KafkaTemplate<String, String> kafkaTemplate;
     private Set<Long> processedProductIds = new HashSet<>();
+
+    private static final String TOPIC_CRITICAL = "critical-stock-notifications";
+    private static final String TOPIC_MODERATE = "moderate-stock-notifications";
+    private static final String TOPIC_LOW = "low-stock-notifications";
 
     @Override
     public void execute() {
@@ -29,20 +32,35 @@ public class CheckCriticalStockGateway implements ICheckCriticalStock {
             .filter(product -> product.stockQuantity() <= product.criticalLevel())
             .filter(product -> !processedProductIds.contains(product.id()))
             .forEach(product -> {
-                sendLowStockNotification(product);
+                sendNotificationBasedOnStock(product);
                 processedProductIds.add(product.id());
             });
     }
 
-    private void sendLowStockNotification(Product product) {
-        String message = String.format("Low stock for product: %s (ID: %s)", product.name(), product.id());
+    private void sendNotificationBasedOnStock(Product product) {
+        String topic = getTopicBasedOnStock(product);
+        String message = String.format("Stock alert for product: %s (ID: %s), Current stock: %d, Critical level: %d",
+                product.name(), product.id(), product.stockQuantity(), product.criticalLevel());
+
         log.info("Notification: {}", message);
-        kafkaTemplate.send(TOPIC, String.valueOf(product.id()), message)
+        kafkaTemplate.send(topic, String.valueOf(product.id()), message)
                 .thenAccept(result -> log.info("Message sent to topic {} with offset {}",
                         result.getRecordMetadata().topic(), result.getRecordMetadata().offset()))
                 .exceptionally(ex -> {
                     log.error("Failed to send message to Kafka", ex);
                     return null;
                 });
+    }
+
+    private String getTopicBasedOnStock(Product product) {
+        int stockDifference = product.criticalLevel() - product.stockQuantity();
+
+        if (stockDifference >= 10) {
+            return TOPIC_CRITICAL;
+        } else if (stockDifference >= 5) {
+            return TOPIC_MODERATE;
+        } else {
+            return TOPIC_LOW;
+        }
     }
 }
